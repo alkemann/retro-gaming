cd47aec265f3c66bc95ac22bd24fbe0058ba45f1
alkemann <alek.morland@gmail.com>
2016-12-21 02:40:02 +0100

    introduced levels

---
+++ snake/levels.js
@@ -0,0 +1,48 @@
+function Level(walls, fruits, start) {
+  return {
+    start: start || createVector(width/2, height/2),
+    fruits: fruits,
+    fruit_left: fruits.length,
+    walls: walls,
+    reset: function() {
+      fruit_left = this.fruits.length;
+      for (var i = 0; i < this.fruits.length; i++) {
+        this.fruits[i].reset();
+        console.log("reset fruit");
+      }
+      console.log(this);
+    },
+    render: function() {
+      for (var i = 0; i < this.walls.length; i++) {
+        this.walls[i].render()
+      }
+      for (var i = 0; i < this.fruits.length; i++) {
+        if (this.fruits[i].eaten) continue
+        this.fruits[i].render();
+        break; // only render first uneaten fruit
+      }
+    }
+  }
+}
+
+function Wall(x, y, width, height) {
+  return {
+    x: x,
+    y: y,
+    cord: pos2cord({x: x, y: y}),
+    width: width,
+    height: height,
+    check: function() {
+      if ((snake.pos.x >= this.x && snake.pos.x <= this.x + this.width)
+          &&
+          (snake.pos.y >= this.y && snake.pos.y <= this.y + this.height)) {
+        console.log("Crashed with WALL!");
+        console.info(this);
+        return true;
+      }
+    },
+    render: function() {
+      Views.wall(this.cord, this.width, this.height);
+    }
+  }
+}
