37391e8ac82222a15beec596b03d402f65edde6c
Alexander Morland <alek.morland@gmail.com>
2017-01-25 18:43:11 +0100

    Render path as line

--- astar/views.js
+++ astar/views.js
@@ -29,13 +29,23 @@ var
             pathfinder.openSet[i].render(color(40, 40, 200));
         }
 
-        var pnode = pathfinder.current;
-        do {
-            pnode.render(color(20, 150, 20));
-            pnode = pnode.parent;
-        } while (pnode != null);
-
         pathfinder.goal.render(color(40, 200, 40));
+
+        var pnode = pathfinder.current;
+        var b = CONF.BLOCK, a = b / 2;
+        while (pnode.parent != null) {
+            // pnode.render(color(20, 150, 20));
+            push();
+            noFill()
+            stroke(100, 200, 100);
+            strokeWeight(10);
+            translate(50, 50);
+
+            line(pnode.x * b + a, pnode.y * b + a, pnode.parent.x * b + a, pnode.parent.y * b + a);
+            pop();
+            pnode = pnode.parent;
+        }
+
 
         for (var x = 0; x < pathfinder.map.length; x++) {
             for (var y = 0; y < pathfinder.map[x].length; y++) {
